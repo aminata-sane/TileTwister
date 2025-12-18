55b0795eefa0421671b716e0906c6b1b9260bad1
aminata-sane <aminata-constance.sane@laplateforme.io>
2025-12-18 12:17:18 +0100

    Refactor: Remplacement du nombre magique 2048 par une constante Grid::TARGET

--- include/Grid.hpp
+++ include/Grid.hpp
@@ -16,6 +16,8 @@ public:   // Ici on mettra les actions possibles (bouger, afficher...)
 
     int getTile(int x, int y) const; // Lecture seule d'une case
 
+    static const int TARGET = 2048;
+
 private: // Ici on met les données cachées (les nombres)
     std::array<std::array<int, 4>, 4> data;
 };
